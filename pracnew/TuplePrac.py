04f88ddb5d04690707a456698d8acb34362cbfe8
vikram <vikramkamboj14@gmail.com>
2025-04-21 20:15:15 +0530

    Tuple

--- pracnew/TuplePrac.py
+++ pracnew/TuplePrac.py
@@ -3,5 +3,14 @@ tup = (3,1,2,4,3,5,3,9,3)
 print(type(tup))
 print(tup)
 print(tup[3:6])
-res = tup.index(3,6)
+res = tup.index(3,2,7)
+print(res)
 print(tup[3:6])
+
+countries = ("Spain", "Italy", "India", "England", "Germany")
+temp = list(countries)
+temp.append("Russia")       #add item
+temp.pop(3)                 #remove item
+temp[2] = "Finland"         #change item
+countries = tuple(temp)
+print(countries)
